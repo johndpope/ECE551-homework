7b4fbbd5b866f51c33fb64a5b3cbc181fe5a97d6
Khoi-Nguyen MAC <knmac@wirelessprv-10-194-136-37.near.illinois.edu>
2016-09-14 16:25:01 -0500

    hw2-code-img

--- homework2/code/main.py
+++ homework2/code/main.py
@@ -1,9 +1,9 @@
+import numpy as np
 from numpy import zeros
 from numpy.linalg import norm
 from numpy.random import rand
-import numpy as np
-import scipy, cv2
 import matplotlib.pyplot as plt
+import scipy, cv2, time
 import ipdb
 
 
@@ -58,7 +58,6 @@ def project_on_tiles(img, tiles):
     pimg = np.zeros(img.shape)
 
     for q in tiles: # That iterates over the fist dimension
-        ipdb.set_trace()
         pimg = pimg + np.dot(q.ravel(), img.ravel())*q
 
     return np.clip(pimg, 0, 255) # Hard threshold to 8uint values
@@ -76,26 +75,50 @@ def main():
     img = np.mean(img_rgb, axis=2) # Average RGB colors to a single channel
     x = img[17] # Choose some random line number as your wish
 
-    for i in range(1,6):
-        proj = project_on_space(x, gen_S(n=1024,N=i).T)
-        #plt.figure(); plt.plot(proj)
+    proj = project_on_space(x, gen_S(n=1024,N=1).T)
+    plt.subplot(2,3,1); plt.plot(proj); plt.xlabel('N=1')
+    proj = project_on_space(x, gen_S(n=1024,N=2).T)
+    plt.subplot(2,3,2); plt.plot(proj); plt.xlabel('N=2')
+    proj = project_on_space(x, gen_S(n=1024,N=3).T)
+    plt.subplot(2,3,3); plt.plot(proj); plt.xlabel('N=3')
+    proj = project_on_space(x, gen_S(n=1024,N=4).T)
+    plt.subplot(2,3,4); plt.plot(proj); plt.xlabel('N=4')
+    proj = project_on_space(x, gen_S(n=1024,N=5).T)
+    plt.subplot(2,3,5); plt.plot(proj); plt.xlabel('N=5')
 
 
     # part c_____________________________________________________________
     H,W = img.shape
 
+    start_time = time.time()
     # Generate circular tiles. Smaller R -> better (and slower) approximation
-    T = gen_circ_tiles(img.shape, 50)
+    T = gen_circ_tiles(img.shape, 30)
     T = np.reshape(T,(T.shape[0], W*H)) # Flatten tiles from I to 0 .. W*H-1
     To = gram_schmidt(T) # This is where your code should run
     To = np.reshape(To,(To.shape[0], H, W)) # Re-rectangle tiles back to I
     pimg = project_on_tiles(img,To) # Project image
+    duration = time.time() - start_time
+    print duration
 
     # Example of plotting images
+    plt.figure()
     plt.subplot(1,2,1)
     plt.imshow(img, interpolation="nearest", cmap="gray")
     plt.subplot(1,2,2)
     plt.imshow(pimg, interpolation="nearest", cmap="gray")
+
+    # color image
+    p_1_img = project_on_tiles(img_rgb[:,:,0],To)
+    p_2_img = project_on_tiles(img_rgb[:,:,1],To)
+    p_3_img = project_on_tiles(img_rgb[:,:,2],To)
+    pimg_rgb = np.stack((p_1_img,p_2_img,p_3_img)).astype(np.uint8)
+    pimg_rgb = pimg_rgb.transpose([1,2,0])
+
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB))
+    plt.subplot(1,2,2)
+    plt.imshow(cv2.cvtColor(pimg_rgb, cv2.COLOR_BGR2RGB))
     plt.show()
     
     return
