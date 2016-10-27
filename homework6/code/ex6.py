de69aff4c18650aac58e6ec423d3d1e910def4fe
knmac <khoinguyenx@gmail.com>
2016-10-26 23:37:09 -0500

    hw6 - add ex6

--- homework6/code/ex6.py
+++ homework6/code/ex6.py
@@ -1,9 +1,19 @@
 import numpy as np
+import sys, ipdb 
+from ex5 import LMS
+
 
 if __name__ == '__main__':
     x = np.loadtxt('data.csv', delimiter=',')
 
-    # Fill in some smart and beautiful Python code here
+    min_mse = sys.maxsize
+    best_w, best_L = 0, 0
+    for L in range(1,100):
+        mu = 1e-3
+        w, x_est = LMS(x, L, mu)
+        mse = np.mean((x-x_est)**2)
+        if mse < min_mse:
+            best_w, best_L = w, L
 
-    print 'The estimated order is: %d' % L
-    print 'The estimated filter: %d' % w
+    print 'The estimated order is: %d' % best_L
+    print 'The estimated filter: %d' % best_w
