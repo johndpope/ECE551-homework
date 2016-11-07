903fa1b78b4e1472bb790683d02cfea27bc5da75
knmac <khoinguyenx@gmail.com>
2016-11-06 19:20:24 -0600

    homework7 - finish coding

--- homework7/code/ex6.py
+++ homework7/code/ex6.py
@@ -24,23 +24,25 @@ def interpolate(t, c, phi):
 
 
 if __name__ == '__main__':
-    # part b-----------------------------------------------------------------------
+    # part b---------------------------------------------------------------------
     phi = [
             lambda t: 1.0*(np.abs(t)<0.5), # phi_0
             lambda t: (1-np.abs(t)) * (np.abs(t)<1), # phi_1
             lambda t: 0.5*(1.5+t)**2 * ((t>=-1.5)*(t<-0.5)) \
                     + (0.75-t**2) * ((t>=-0.5)*(t < 0.5)) \
                     + 0.5*(1.5-t)**2 * ((t>=0.5)*(t<1.5)), # phi_2
-            lambda t: np.cos(np.pi*t) * (np.abs(t)<0.5), # phi_3
-            lambda t: np.cos(np.pi/2*t) * (np.abs(t)<1) # phi_4
+            lambda t: 0.5*(1.0+np.cos(np.pi*t)) * (np.abs(t)<1.0), # phi_3
+            lambda t: np.cos(np.pi*t) * (np.abs(t)<0.5) # phi_4
             ]
 
+    mu = np.sqrt(8/(2*np.sqrt(2)+3))
+    gamma = 2*np.sqrt(2)-3
     filters = [
             lambda x: x,
-            lambda x: x, #TODO
-            lambda x: fwd_bwd_filter(1.2, -0.2, x), #TODO: change mu, gamma
-            lambda x: x, #TODO
-            lambda x: x, #TODO
+            lambda x: x,
+            lambda x: fwd_bwd_filter(mu, gamma, x),
+            lambda x: x,
+            lambda x: x, 
             ]
     
     x = [6,7,5,6,9,2,4,3,6]
@@ -54,31 +56,30 @@ if __name__ == '__main__':
         plt.subplot(len(phi), 1, k+1)
         plt.plot(t, s)
         plt.plot(np.arange(9), x, 'rx')
+        plt.ylabel('phi'+str(k))
 
 
-    # part c----------------------------------------------------------------------
+    # part c---------------------------------------------------------------------
     N = 5 # number of points
     plt.figure() # open a figure
     plt.axis([0,1,0,1]) # ... and a axis
     plt.grid('on')
     points = np.array(plt.ginput(N)) # pick N points using mouse input
     plt.plot(points.T[0], points.T[1], 'rx') # plot them
+    plt.close()
+    
 
-
-    '''
-    t = np.array([])
-    for n in range(N-1):
-        new_segment = np.arange(points.T[0][n], points.T[0][n+1], 0.01)
-        t = np.concatenate((t, new_segment), axis=0)
-    ipdb.set_trace()
-    '''
-    t = np.linspace(0,N+1,100)
-    c = filters[1](points.T[1])
-    s = interpolate(t, c, phi[1])
-
+    t = np.linspace(0,N-1,500)
     plt.figure()
-    plt.plot(t, s, 'b-')
-    plt.plot(points.T[0], points.T[1], 'rx')
-    plt.grid('on')
+    for k in range(len(phi)):
+        c0 = filters[k](points.T[0])
+        c1 = filters[k](points.T[1])
+        s0 = interpolate(t, c0, phi[k])
+        s1 = interpolate(t, c1, phi[k])
+
+        plt.plot(s0, s1, label='phi'+str(k))
+
+    plt.plot(points.T[0], points.T[1], 'o')
+    plt.axis([0,1,0,1]); plt.grid('on'); plt.legend()
 
     plt.show()
